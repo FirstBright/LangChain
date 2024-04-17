be822d467515b4d17bae6bca06ae1e3a571a24fc
Antonio.Choi <nmbertt1@naver.com>
2024-04-17 14:44:02 +0900

    api -> env

--- gemini.py
+++ gemini.py
@@ -2,13 +2,15 @@ import os
 import requests
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = "API 입력 예정"
+from dotenv import load_dotenv
 
 class GeminiClient:
     def __init__(self, model='gemini-pro', temperature=1.0):
         self.chat_engine = ChatGoogleGenerativeAI(model=model, temperature=temperature) # chat engine으로 chat 클래스와 구분
+        api = load_dotenv()
+        if "GOOGLE_API_KEY" not in os.environ:
+            os.environ["GOOGLE_API_KEY"] = api
+
         self.prompt = ChatPromptTemplate.from_messages([
             ('system', 'Answer like lawyer.'),
             MessagesPlaceholder(variable_name='message')
