70249915d2143a139559d403cfb65bb57f7ae08b
jaejun.yang <iam_idea@naver.com>
2024-04-18 20:39:12 +0000

    4차 수정 및 test.py 삭제

---
+++ main.py
@@ -0,0 +1,18 @@
+from chat import Chat, Chat_SQL
+import os
+
+def main():
+    os.environ.setdefault("GOOGLE_API_KEY", "Your_API_Key")
+    question_text = input("질문을 입력하세요: ")
+
+    chat = Chat()   
+    response = chat.ask(question_text)
+    print(f"{response}")
+    
+    chat_sql = Chat_SQL()
+    response = chat_sql.ask(question_text)
+    print(f"{response}")
+
+if __name__ == "__main__":
+    main()
+
