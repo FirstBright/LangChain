d6ada8259cbfe49b3998bf9142e466f2dcf2abcc
FirstBright <mipo8890@gmail.com>
2024-04-19 02:29:35 +0000

    make logger class, use it in main

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from chat import Chat, Chat_SQL
 import os
-
+from logger import Logger
 def main():
-    os.environ.setdefault("GOOGLE_API_KEY", "Your_API_Key")
-    question_text = input("질문을 입력하세요: ")
-
-    chat = Chat()   
-    response = chat.ask(question_text)
-    print(f"{response}")
-    
+    logger = Logger()
+    chat = Chat()  
     chat_sql = Chat_SQL()
-    response = chat_sql.ask(question_text)
-    print(f"{response}")
+    os.environ.setdefault("GOOGLE_API_KEY", "Your_API_Key")
+    for i in range(10):
+        question_text = input("질문을 입력하세요: ")    
+        logger.log_message(question_text)     
+        response = chat.ask(question_text)
+        print(f"{response}")    
+        print('='*40)
+        response = chat_sql.ask(question_text)
+        print(f"{response}")
 
 if __name__ == "__main__":
     main()
