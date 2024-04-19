f9811a0d6bb08e537ba4d07f705a4a318941bc2a
FirstBright <mipo8890@gmail.com>
2024-04-19 02:52:19 +0000

    logger main

--- logger.py
+++ logger.py
@@ -1,9 +1,7 @@
 import os
 import logging
 from datetime import datetime
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log', filemode='a')
-
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
